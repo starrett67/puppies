6870bcc6956448f4e164d4b21b387a67962a9454
starrett67 <starrett67@gmail.com>
2013-11-19 16:51:14 -0500

    debug

--- puppies/Scripts/YoutubeSearch.js
+++ puppies/Scripts/YoutubeSearch.js
@@ -13,9 +13,12 @@ function search(val, container) {
     });
 
     request.execute(function (response) {
+        console.log(response);
         var str = JSON.stringify(response.result);
         console.log(str);
-        var videoID = response.results.items[0];
+        var jsonObj = JSON.parse(str);
+        console.log(jsonObj);
+        var videoID = jsonObj.items[0].videoId;
         console.log(videoID);
         $('#' + container).html('<iframe width="420" height="315" src="//www.youtube.com/embed/' + videoID + '" frameborder="0" allowfullscreen></iframe>');
     });
